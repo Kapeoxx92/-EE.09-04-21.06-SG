bec812212635ad525fd171ab5beb9762decc85ff
1698551 <1698551@TE-GDA-D-D21E10>
2024-12-16 10:35:50 +0100

    commit3

--- users.php
+++ users.php
@@ -13,13 +13,40 @@ function people($server, $username, $password, $db) {
     $sql = "SELECT id, imie, nazwisko, rok_urodzenia, zdjecie FROM osoby LIMIT 30;";
     
     $result = mysqli_query($conn, $sql);
+    
+    
+
     if(mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
             echo $row["id"];
+            echo ". ";
+            echo $row["imie"];
+            echo " ";
+            echo $row["nazwisko"];
+            echo " ";
+            $rok = 2024;
+            $wiek = $rok - $row["rok_urodzenia"];
+            echo $wiek;
+            echo " ";
+            echo "lat";
+            echo "<br>";
     }
+
     mysqli_close($conn);
+    }
 }
-};
+    function person($server, $username, $password, $db) {
+    $conn2 = mysqli_connect($server, $username, $password, $db);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql2 = "SELECT imie, nazwisko, rok_urodzenia, opis, zdjecie FROM osoby, hobby WHERE osoby.Hobby_id = hobby.id AND osoby.id = ;";
+
+    $result2 = mysqli_query($conn, $sql);
+
+    mysqli_close($conn);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -51,7 +78,9 @@ function people($server, $username, $password, $db) {
                 <input type="submit" value="ZOBACZ" id="send">
             </form>
             <hr>
-            skrypt2
+            <?php
+            person($server, $username, $password, $db)
+            ?>
         </section>
     </main>
     <footer>Stronę wykonał: 00000000000</footer>
