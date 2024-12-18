dcb9294e379e49bac9714bf1f3c2a9017b815e6c
Kapeoxx92 <kacperwojtczuk92@gmail.com>
2024-12-18 15:38:49 +0100

    commitFinal

--- users.php
+++ users.php
@@ -1,23 +1,24 @@
 <?php
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $db = 'dane4';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'dane4';
 
-function people($server, $username, $password, $db) {
+function people($server, $username, $password, $db)
+{
     $conn = mysqli_connect($server, $username, $password, $db);
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
     $sql = "SELECT id, imie, nazwisko, rok_urodzenia, zdjecie FROM osoby LIMIT 30;";
-    
-    $result = mysqli_query($conn, $sql);
-    
-    
 
-    if(mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)) {
+    $result = mysqli_query($conn, $sql);
+
+
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
             echo $row["id"];
             echo ". ";
             echo $row["imie"];
@@ -30,23 +31,34 @@ function people($server, $username, $password, $db) {
             echo " ";
             echo "lat";
             echo "<br>";
-    }
+        }
 
-    mysqli_close($conn);
+        mysqli_close($conn);
     }
 }
-    function person($server, $username, $password, $db) {
-    $conn2 = mysqli_connect($server, $username, $password, $db);
+function person($server, $username, $password, $db)
+{
+    if (isset($_POST["user_id"]) && strlen($_POST["user_id"]) > 0) {
+        $conn = mysqli_connect($server, $username, $password, $db);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $person_id = $_POST["user_id"];
+        $sql = "SELECT imie, nazwisko, rok_urodzenia, opis, zdjecie, nazwa FROM osoby, hobby WHERE osoby.Hobby_id = hobby.id AND osoby.id = $person_id;";
+
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+?>
+        <h2><?= $_POST["user_id"] ?>. <?= $row["imie"] ?> <?= $row["nazwisko"] ?></h2>
+        <img src="<?= $row["zdjecie"] ?>" alt="<?= $_POST["user_id"] ?>">
+        <p>Rok urodzenia: <?= $row["rok_urodzenia"] ?></p>
+        <p>Opis: <?= $row["opis"] ?></p>
+        <p>Hobby: <?= $row["nazwa"] ?></p>
+<?php
+        mysqli_close($conn);
     }
-    $sql2 = "SELECT imie, nazwisko, rok_urodzenia, opis, zdjecie FROM osoby, hobby WHERE osoby.Hobby_id = hobby.id AND osoby.id = ;";
-
-    $result2 = mysqli_query($conn, $sql);
-
-    mysqli_close($conn);
-    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -74,7 +86,7 @@ function people($server, $username, $password, $db) {
         <section id="right">
             <h4>Podaj id użytkownika</h4>
             <form action="users.php" method="post">
-                <input type="number">
+                <input type="number" name="user_id">
                 <input type="submit" value="ZOBACZ" id="send">
             </form>
             <hr>
